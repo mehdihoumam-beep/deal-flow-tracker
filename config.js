348c40e6afd59a44c8b4888d9d984211efa25cf1
mehdihoumam-beep <mehdi.houmam@gmail.com>
2026-07-19 22:06:33 +0100

    Update CLIENT_ID in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
    Voir GUIDE-INSTALLATION.md pour la marche a suivre complete. */
 window.DEAL_FLOW_CONFIG = {
   // Colle ici le "Application (client) ID" recupere dans Microsoft Entra > Inscriptions d'applications.
-  CLIENT_ID: "COLLE_TON_CLIENT_ID_ICI",
+  CLIENT_ID: "7a925922-25d3-4dee-823f-7e6b1eb2f11b",
 
   // Ne pas modifier : restreint la connexion aux comptes Microsoft personnels.
   AUTHORITY: "https://login.microsoftonline.com/consumers",
